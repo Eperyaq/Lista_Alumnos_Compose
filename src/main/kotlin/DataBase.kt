ad1c54a18f02c49b8581685c2dd2ec587e688700
elia_clase <eperyaq205@g.educaand.es>
2024-05-03 21:22:44 +0200

    Object DataBase creado falta añadir metodos

--- src/main/kotlin/DataBase.kt
+++ src/main/kotlin/DataBase.kt
@@ -1,4 +1,5 @@
 import androidx.compose.ui.unit.Constraints
+import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
 
@@ -13,13 +14,43 @@ object DataBase {
     }
 
     data class Estudiante(val nombre: String, val id:Int) //Crear la "tabla"
-    val conexion = DriverManager.getConnection(URL, USUARIO, CONTRASENIA) //Dar conexion con la base de datos
+
+    /**
+     * Funcion que establece la conexion con la base de datos
+     * @return La conexion con la base de datos
+     */
+    fun connectDB(): Connection?{
+        var conexion : Connection? = null
+
+        try {
+            conexion = DriverManager.getConnection(URL, USUARIO, CONTRASENIA) //Dar conexion con la base de datos
+        } catch ( e: SQLException){
+            e.printStackTrace()
+        }
+
+        return conexion
+    }
+
+    /**
+     * Funcion que cierra la database
+     * @param conexion Conexion que hay que cerrar
+     */
+    fun closeDB(conexion:Connection?) {
+        try {
+            conexion?.close()
+        } catch (e:SQLException){
+            e.printStackTrace()
+        }
+    }
+
 
     fun addStudent() {
+        val conexion = connectDB()
+
         try {
 
             println("Conexión exitosa")
-            conexion.close()
+            conexion?.close()
         } catch (e: SQLException) {
             println("Error en la conexión: ${e.message}")
         } catch (e: ClassNotFoundException) {
@@ -28,14 +59,30 @@ object DataBase {
     }
 
     fun selectAllStudents(){
+
+        val conexion = connectDB()
+        val query = "SELECT * FROM STUDENTS"
+        val listaEstudiante = mutableListOf<Estudiante>()
+
         try {
-            val estatement = conexion.prepareStatement("SELECT * FROM STUDENTS")
+            val statement = conexion?.createStatement() //Sirve para crear una query
+            val resultado = statement?.executeQuery(query) //Esto te va a ejecutar la query y va a preparar el resultado
+
+            while (resultado?.next() == true){
+
+                val idStudent = resultado.getInt("id")//Saca el id y lo mete en la tabla
+                val nameStudent = resultado.getString("nombre") //Este saca el nombre y lo mete en la tabla
+                listaEstudiante.add(Estudiante(nameStudent, idStudent))
+            }
+            resultado?.close()
+            statement?.close()
 
         } catch (e: SQLException){
-            println("Error")
+            e.printStackTrace()
+        }  finally {
+            closeDB(conexion)
         }
 
     }
-
 
 }
